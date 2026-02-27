2bd1c5e6d61004fb8c186160fb2873b8db2cf219
pma152402 <168718903+pma152402@users.noreply.github.com>
2026-02-27 22:32:06 +0100

    feat: folder name refactor

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import RegisterView from '../views/RegisterView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'register',
+      component: RegisterView
     }
   ],
 })
