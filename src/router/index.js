d890acf2d7b2f3b5b5908d1d12e384a3d0d1a8ec
pma152402 <168718903+pma152402@users.noreply.github.com>
2026-03-01 19:00:40 +0100

    feat: Actualizacion router

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import RegisterView from '../views/RegisterView.vue'
+import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
+      path: '/register',
       name: 'register',
       component: RegisterView
+    },
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
     }
   ],
 })
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup>
+</script>
+
+<template>
+    <div
+    class="bg-gradient-to-t from-gray-400/50 to-gray-300/50 h-screen flex items-center justify-center"
+  >
+  </div>
+</template>
