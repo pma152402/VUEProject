a7ac26d6be928b0fb76f83de5b82dc6a1c4647ff
pma152402 <168718903+pma152402@users.noreply.github.com>
2026-03-02 13:52:27 +0100

    feat: Actualización landing

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,15 +5,15 @@ import logo from "../assets/logo_3.png";
 <template>
   <div class="bg-gradient-to-t from-gray-400/50 to-gray-300/50 h-screen overflow-x-hidden">
     <div class="flex flex-col max-w-7xl items-center mx-auto pt-50 lg:pt-20">
-      <div class="bg-neutral-400 relative rotate-3 rounded-4xl shadow-blue-300 shadow-2xl">
-        <div class="bg-neutral-500 relative -rotate-1 rounded-4xl shadow-2xl">
+      <div class="bg-blue-200 relative rotate-3 rounded-4xl">
+        <div class="bg-blue-100 relative -rotate-1 rounded-4xl">
           <div
-            class="flex flex-col items-center justify-center bg-gradient-to-r from-neutral-100/90 to-blue-50 pb-10 rounded-4xl -rotate-2 shadow-2xl"
+            class="flex flex-col items-center justify-center bg-gradient-to-r from-neutral-100/90 to-blue-50 pb-10 rounded-4xl -rotate-2 shadow-2xl border border-gray-400 hover:scale-108 transition-all ease-in-out"
           >
             <img :src="logo" class="w-lg lg:w-2xl" />
 
             <h1
-              class="text-5xl lg:text-6xl font-extrabold pb-5 text-gray-900 mt-[-40px] lg:mt-[-90px]"
+              class="text-5xl lg:text-6xl font-extrabold pb-5 text-gray-800 mt-[-40px] lg:mt-[-90px]"
             >
               ORGANIZER
             </h1>
@@ -28,7 +28,7 @@ import logo from "../assets/logo_3.png";
               >
               <RouterLink to="/register">
                 <button
-                  class="rounded-full bg-blue-400/80 px-2 py-1 font-md text-lg transition-transform duration-200 ease-in-out hover:scale-105 hover:bg-blue-500/40 hover:cursor-pointer hover:font-semibold"
+                  class="rounded-full bg-blue-400/80 px-2 py-1 font-md text-lg transition-transform duration-200 ease-in-out hover:scale-105 hover:bg-blue-400 hover:cursor-pointer hover:font-semibold"
                 >
                   Registro
                 </button></RouterLink
