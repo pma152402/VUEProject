692873304a34db7a68d74d9615ab0f0eaa2594bb
pma152402 <168718903+pma152402@users.noreply.github.com>
2026-03-04 23:02:25 +0100

    feat: Panel protection

--- src/router/index.js
+++ src/router/index.js
@@ -1,33 +1,43 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import RegisterView from '../views/RegisterView.vue'
-import LoginView from '../views/LoginView.vue'
-import HomeView from '../views/HomeView.vue'
-import PanelView from '../views/PanelView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import RegisterView from "../views/RegisterView.vue";
+import LoginView from "../views/LoginView.vue";
+import HomeView from "../views/HomeView.vue";
+import PanelView from "../views/PanelView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/register',
-      name: 'register',
-      component: RegisterView
+      path: "/register",
+      name: "register",
+      component: RegisterView,
     },
     {
-      path: '/login',
-      name: 'login',
-      component: LoginView
+      path: "/login",
+      name: "login",
+      component: LoginView,
     },
     {
-      path: '/panel',
-      name: 'panel',
-      component: PanelView
+      path: "/panel",
+      name: "panel",
+      component: PanelView,
+      meta: { requiresAuth: true },
     },
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    }
+      path: "/",
+      name: "home",
+      component: HomeView,
+    },
   ],
-})
+});
+router.beforeEach((to, from, next) => {
+  const usuario = localStorage.getItem("usuario");
 
-export default router
+  if (to.meta.requiresAuth && !usuario) {
+    next("/login");
+  } else {
+    next();
+  }
+});
+
+export default router;
