11d9a0b89bbd925e428af2963dc80d5b04bdf1e0
pma152402 <pma152402@gmail.com>
2026-03-06 16:30:58 +0100

    feat: ProjectView mockup

--- src/views/ProjectView.vue
+++ src/views/ProjectView.vue
@@ -3,12 +3,21 @@ import { onMounted, ref } from "vue";
 import { useRoute } from "vue-router";
 
 const route = useRoute();
+const usuario = ref(null);
 const proyecto = ref(null);
 const IDproyecto = route.params.id;
 
 // en el montaje..
 onMounted(async () => {
+  // cargar proyecto
   proyecto.value = await cargarProyecto(IDproyecto);
+
+  // cargar usuario
+  const usuarioGuardado = localStorage.getItem("usuario");
+
+  if (usuarioGuardado) {
+    usuario.value = JSON.parse(usuarioGuardado);
+  }
 });
 
 // cargar el proyecto con la id de la ruta
@@ -43,19 +52,40 @@ async function cargarProyecto(IDproyecto) {
 </script>
 <template>
   <div class="bg-gradient-to-t from-gray-400/50 to-gray-300/50 h-screen overflow-x-hidden">
-    <div class="text-5xl lg:text-6xl flex pb-5 text-gray-800 p-10">
+    <div class="text-5xl lg:text-6xl flex flex-col md:flex-row pb-5 text-gray-800 p-10">
       <span>Proyecto: </span>
       <h1 v-if="proyecto" class="font-bold pl-4">{{ proyecto.name }}</h1>
     </div>
-    <div v-if="proyecto" class="p-10 text-lg">
-      <p>Creado el: {{ new Date(Number(proyecto.createdAt)).toLocaleDateString("es-ES") }}</p>
+    <div class="p-10 text-lg">
+      <div class="flex gap-2">
+        <p class="font-semibold">Descripción:</p>
+        <p>Aún no puedes modificar las descripciones de tus proyectos</p>
+      </div>
+      <div class="flex gap-2">
+        <p class="font-semibold">Miembros:</p>
+        <p v-if="usuario">{{ usuario.name }}</p>
+      </div>
 
-      <p>Descripción: (toDo)</p>
-
-      <p>Miembros: (toDo)</p>
+      <div class="flex gap-2">
+        <p class="font-semibold">Creado el:</p>
+        <p v-if="proyecto">
+          {{ new Date(Number(proyecto.createdAt)).toLocaleDateString("es-ES") }}
+        </p>
+      </div>
     </div>
-    <div class="bg-amber-200 flex items center justify-center">
-      <div class="bg-white px-4 py-2 rounded-xl"><span class="text-2xl font-semibold">Tarjeta 1</span></div>
+    <div class="bg-gray-100 flex items center justify-center gap-6">
+      <div class="bg-white px-4 py-2 rounded-xl">
+        <span class="text-2xl font-semibold border-b ">Tarjeta 1</span>
+        <ul class="AQUI">
+          <li class="mb-2">Lo primero</li>
+          <li class="mb-2">Lo segundo</li>
+          <li class="mb-2">Lo tercero</li>
+        </ul>
+        <button class="rounded-xl px-2 py-1 mt-2 bg-neutral-200">+ Añadir tarea</button>
+      </div>
+      <div class="bg-neutral-400/80 px-4 py-2 rounded-xl max-h-12">
+        <span class="text-2xl font-semibold">+ Nueva tarjeta</span>
+      </div>
     </div>
   </div>
 </template>
