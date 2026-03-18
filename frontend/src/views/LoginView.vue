fc3eb4b8725c5d1c227b79f7e4b651bad7ebbd29
pma152402 <168718903+pma152402@users.noreply.github.com>
2026-03-19 00:34:17 +0100

    feature/LoadingLogin DONE

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { ref, watch } from "vue";
-import { House } from "lucide-vue-next";
+import { House, LoaderCircle } from "lucide-vue-next";
 
 import Login from "../components/login/Login.vue";
 
@@ -23,31 +23,41 @@ const errorContrasena = ref("");
 const paso = ref(1);
 
 // Función para iniciar sesión
+const cargando = ref(false);
+
 async function iniciarSesion() {
-  const usuario = await login(email.value, contrasena.value);
+  cargando.value = true;
 
-  // errores usuario
-  if (usuario?.error) {
-    if (usuario.error.includes("contraseña")) {
-      errorContrasena.value = usuario.error;
+  try {
+    const usuario = await login(email.value, contrasena.value);
+
+    // errores usuario
+    if (usuario?.error) {
+      if (usuario.error.includes("contraseña")) {
+        errorContrasena.value = usuario.error;
+      }
+
+      if (usuario.error.includes("usuario")) {
+        errorEmail.value = usuario.error;
+      }
+
+      return;
     }
 
-    if (usuario.error.includes("usuario")) {
-      errorEmail.value = usuario.error;
+    // si pasa se guarda en localStorage
+    if (usuario) {
+      localStorage.setItem("usuario", JSON.stringify(usuario));
+
+      paso.value = 2;
+
+      setTimeout(() => {
+        router.push("/panel");
+      }, 1500);
     }
-
-    return;
-  }
-
-  // si pasa se guarda en localStorage
-  if (usuario) {
-    localStorage.setItem("usuario", JSON.stringify(usuario));
-
-    paso.value = 2;
-
-    setTimeout(() => {
-      router.push("/panel");
-    }, 1500);
+  } catch (error) {
+    console.log(error);
+  } finally {
+    cargando.value = false;
   }
 }
 
@@ -60,7 +70,6 @@ watch(contrasena, () => {
 });
 
 //  controlar el formulario
-
 function comprobarLogin() {
   checkLogin({
     email,
@@ -71,25 +80,45 @@ function comprobarLogin() {
 }
 </script>
 
+<style>
+ /* Animacion de carga */
+ @keyframes girar {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+}
+
+  .circuloDeCarga {
+    animation: girar 2s linear infinite;
+  }
+</style>
+
 <template>
   <div
     class="bg-gradient-to-t from-gray-400/50 to-gray-300/50 h-screen flex items-center justify-center"
   >
-    <div
-      class="bg-gradient-to-t from-blue-300 to-blue-200/80 rounded-2xl flex items-center"
-    >
+    <div class="bg-gradient-to-t from-blue-300 to-blue-200/80 rounded-2xl flex items-center">
       <form @submit.prevent="iniciarSesion">
         <div
           class="relative ml-3 rounded-lg bg-neutral-100 flex flex-col items-center px-8 pt-8 pb-4 gap-5 shadow-xl"
         >
+          <!-- Volver a home -->
           <RouterLink to="/">
             <a
-              v-if="paso === 1"
               class="text-gray-400 absolute bottom-4 left-8 mr-20 font-semibold flex flex-nowrap transition-transform duration-200 ease-in-out hover:scale-105 hover:cursor-pointer"
             >
               < <House class="w-4" />
             </a>
           </RouterLink>
+          <!-- Circulo de carga -->
+          <a v-if="cargando"
+            class=" text-gray-400 absolute bottom-4 right-0 mr-20 font-semibold flex flex-nowrap animate-spin"
+          >
+            <LoaderCircle class="w-5"/>  
+          </a>
           <div class="flex-col items-center mb-6">
             <h1 class="font-light text-2xl text-gray-900">Inicia sesión en</h1>
             <h1 class="font-bold text-5xl text-gray-900">ORGANIZER</h1>
@@ -111,7 +140,8 @@ function comprobarLogin() {
               class="rounded-full bg-blue-200/80 px-2 py-1 font-md text-sm transition-transform duration-200 ease-in-out hover:scale-105 hover:bg-blue-500/40 hover:cursor-pointer hover:font-semibold"
               v-if="paso === 1"
             >
-              Iniciar sesión
+              <span v-if="!cargando">Iniciar sesión</span>
+              <span v-else>Iniciando sesión..</span>
             </button>
             <!-- </RouterLink> -->
           </div>
