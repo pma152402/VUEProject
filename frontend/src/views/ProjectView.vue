e81baec429bf8062e5732eedb161c63357b9b74b
pma152402 <168718903+pma152402@users.noreply.github.com>
2026-03-25 18:15:48 +0100

    feat: Bugfix color

--- frontend/src/views/ProjectView.vue
+++ frontend/src/views/ProjectView.vue
@@ -291,7 +291,7 @@ async function moverTarjeta(evt) {
 
 
 <template>
-  <div class="bg-neutral-300  min-h-screen flex justify-center relative">
+  <div class="bg-neutral-300  min-h-screen flex justify-center relative ">
 
     <!-- HACER MODALES.VUE-->
     <!-- a) Borrar tarjeta -->
