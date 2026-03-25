ef073842d881b7d32f68268c299129cd245a8c0f
pma152402 <168718903+pma152402@users.noreply.github.com>
2026-03-25 18:14:22 +0100

    feat: Color brand correction bg-neutral-300

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -102,7 +102,7 @@ async function comprobarLogin() {
 
 <template>
   <div
-    class="bg-gradient-to-t from-gray-400/50 to-gray-300/50 h-screen flex items-center justify-center"
+    class="bg-neutral-300  h-screen flex items-center justify-center"
   >
     <div class="bg-gradient-to-t from-blue-300 to-blue-200/80 rounded-2xl flex items-center">
       <form @submit.prevent="comprobarLogin">
