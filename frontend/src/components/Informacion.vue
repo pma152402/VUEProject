2d9b6d9d73b2bd70fd59bf2339eef97eb27330cc
pma152402 <168718903+pma152402@users.noreply.github.com>
2026-03-26 02:15:33 +0100

    feat: Guia rapida, pasos 1 y 2

--- frontend/src/components/Informacion.vue
+++ frontend/src/components/Informacion.vue
@@ -1,6 +1,9 @@
 <script setup>
 import { BadgeQuestionMark } from "lucide-vue-next";
 import { ref } from "vue";
+import aniadir_tarjeta from "../assets/aniadir_tarjeta.png";
+import aniadir_tarea from "../assets/aniadir_tarea.png";
+import pet from "../assets/pet.png";
 
 const paso = ref(0);
 </script>
@@ -34,7 +37,7 @@ const paso = ref(0);
   <div v-if="paso >= 1" class="fixed inset-0 z-3000 bg-neutral-900/70 w-full h-full">
     <!-- Guia -->
     <div
-      class="bg-neutral-200 rounded-lg px-6 pt-6 pb-4 flex flex-col items-center absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+      class="bg-neutral-200 rounded-lg px-8 pt-6 pb-4 flex flex-col items-start absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-130"
     >
       <!-- X -->
       <div @click="paso = 0" class="absolute top-1 right-4 text-xl hover:cursor-pointer">x</div>
@@ -43,28 +46,42 @@ const paso = ref(0);
         <h1 class="text-5xl font-bold">Guía rápida:</h1>
 
         <!-- Primer paso-->
-        <p 
-        v-if="paso == 1"
-        class="max-w-md mt-6 border-l-4 border-blue-400 pl-2 pb-1.5 text-lg leading-relaxed">
-          Bienvenido a tu proyecto, aquí puedes crear todas las tarjetas y tareas que necesites
-        </p>
-        <!-- Segundo paso-->
-        <p 
-        v-if="paso == 2"
-        class="max-w-md mt-6 border-l-4 border-blue-400 pl-2 pb-1.5 text-lg leading-relaxed">
-          Este es el paso dos
-        </p>
 
+        <div v-if="paso == 1">
+          <div class="max-w-md mt-8 border-l-4 border-blue-400 pl-2 pb-0.5 text-lg leading-relaxed">
+            <p class="inline">Bienvenido a tu proyecto, aquí puedes crear todas las </p>
+            <p class="inline font-semibold">tarjetas y tareas </p>
+            <p class="inline">que necesites:</p>
+          </div>
+
+          <div class="mt-6 w-full pr-10 pl-15 flex justify-between font-light text-sm italic">
+            <div>
+              En la barra de acciones:
+
+              <img class="rounded-md shadow-md" :src="aniadir_tarjeta" />
+            </div>
+            <div>
+              En cada tarjeta:
+
+              <img class="rounded-md shadow-md" :src="aniadir_tarea" />
+            </div>
+          </div>
+        </div>
+
+        <!-- Segundo paso-->
+        <div v-if="paso == 2">
+          <div class="mb-6 max-w-md mt-8 border-l-4 border-blue-400 pl-2 text-lg leading-relaxed">
+            <p class="inline font-semibold">Plantilla PET: </p>
+            <p class="inline">Crea un layout básico de tres tarjetas</p>
+          </div>
+          <img class="rounded-xl shadow-md mb-2" :src="pet" />
+        </div>
       </div>
 
       <div class="w-full items-center flex justify-between px-20 mt-6">
-        <span 
-        @click="paso = paso - 1"
-        class="text-gray-400 hover:cursor-pointer"><</span>
-        <span class="hover:cursor-default">{{paso}}/3</span>
-        <span 
-        @click="paso = paso + 1"
-        class="hover:cursor-pointer">></span>
+        <span @click="paso = paso - 1" class="text-gray-400 hover:cursor-pointer"><</span>
+        <span class="hover:cursor-default">{{ paso }}/3</span>
+        <span @click="paso = paso + 1" class="hover:cursor-pointer">></span>
       </div>
     </div>
   </div>
